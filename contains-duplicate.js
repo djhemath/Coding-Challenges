cfdbc5774ef77b74607cea5e73f79a92262e048c
DJ Hemath <djhemath@gmail.com>
2023-12-24 07:26:01 +0530

    solution(contains-duplicate): one liner

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -21,3 +21,12 @@ function containsDuplicate(nums) {
 console.log(containsDuplicate([1,2,3,1]) === true);
 console.log(containsDuplicate([1,2,3,4]) === false);
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]) === true);
+
+
+function containsDuplicateOneLiner(nums) {
+  return (new Set(nums).size) !== nums.length;
+}
+
+console.log(containsDuplicateOneLiner([1,2,3,1]) === true);
+console.log(containsDuplicateOneLiner([1,2,3,4]) === false);
+console.log(containsDuplicateOneLiner([1,1,1,3,3,4,3,2,4,2]) === true);
