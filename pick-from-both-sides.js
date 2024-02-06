3fc8187d847c488accd6f0e9e7f3c57fb8ef0816
DJ Hemath <djhemath@gmail.com>
2024-02-06 20:40:28 +0530

    solution(pick-from-both-sides): space optimized solution

--- pick-from-both-sides.js
+++ pick-from-both-sides.js
@@ -31,3 +31,36 @@ function maxSum(A, B){
 console.log(maxSum([5, -2, 3 , 1, 2], 3) === 8);
 console.log(maxSum([1, 2], 1) === 2);
 console.log(maxSum([5, -2, 3 , 1, 2], 5) === 9);
+
+// T -> O(B)
+// S -> O(1)
+function maxSum2(A, B) {
+  let sum = 0;
+
+  for(let i=0; i<B; i++) {
+    sum += A[i];
+  }
+
+  let maxSum = sum;
+
+  let i = B - 1;
+  let j = A.length - 1;
+
+  while(i >= 0) {
+    sum -= A[i];
+    i -= 1;
+
+    sum += A[j];
+    j -= 1;
+
+    if(sum > maxSum) {
+      maxSum = sum;
+    }
+  }
+
+  return maxSum;
+}
+
+console.log(maxSum2([5, -2, 3 , 1, 2], 3) === 8);
+console.log(maxSum2([1, 2], 1) === 2);
+console.log(maxSum2([5, -2, 3 , 1, 2], 5) === 9);
